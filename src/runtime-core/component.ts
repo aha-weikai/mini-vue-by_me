ad7cb3b2a9a3f4c0e57b757ba9604a0e3a5766d8
aha <1900461953@qq.com>
2023-01-04 23:50:40 +0800

    feat: 初始化component的流程

---
+++ src/runtime-core/component.ts
@@ -0,0 +1,37 @@
+export function createComponentInstance(vnode) {
+  const component = { vnode, type: vnode.type };
+
+  return component;
+}
+
+export function setupComponent(instance) {
+  // initProps()
+  // initSlots()
+  setupStatefulComponent(instance);
+}
+
+function setupStatefulComponent(instance: any) {
+  const component = instance.type;
+
+  const { setup } = component;
+
+  if (setup) {
+    const setupResult = setup();
+
+    handleSetupResult(instance, setupResult);
+  }
+}
+function handleSetupResult(instance, setupResult: any) {
+  // function Object
+  if (typeof setupResult === "object") {
+    instance.setupState = setupResult;
+  }
+  finishComponentSetup(instance);
+}
+function finishComponentSetup(instance: any) {
+  const component = instance.type;
+
+  if (component.render) {
+    instance.render = component.render;
+  }
+}
---
+++ src/runtime-core/createApp.ts
@@ -0,0 +1,14 @@
+import { render } from "./renderer";
+import { createVNode } from "./vnode";
+
+export function createApp(rootComponent) {
+  return {
+    mount(rootContainer) {
+      // rootComponent => vnode
+      // 所有的逻辑操作都基于 vnode 处理
+
+      const vnode = createVNode(rootComponent);
+      render(vnode, rootContainer);
+    },
+  };
+}
