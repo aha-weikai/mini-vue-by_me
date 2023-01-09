c59e9615a3bf99d4c738091fbd406a0c92880f44
aha <1900461953@qq.com>
2023-01-09 10:41:01 +0800

    feat: mountElement实现

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,4 +1,7 @@
+import { isObject } from "../shared/index";
+import { ShapeFlags } from "../shared/shapeFlags";
 import { createComponentInstance, setupComponent } from "./component";
+import { createVNode } from "./vnode";
 
 export function render(vnode, container) {
   patch(vnode, container);
@@ -7,25 +10,62 @@ export function render(vnode, container) {
 function patch(vnode: any, container: any) {
   // processElement()
   // 处理组件
-  processComponent(vnode, container);
+  const { shapeFlag } = vnode;
+  if (shapeFlag & ShapeFlags.ELEMENT) {
+    processElement(vnode, container);
+  } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
+    processComponent(vnode, container);
+  }
 }
 
 function processComponent(vnode: any, container: any) {
   mountComponent(vnode, container);
 }
 
-function mountComponent(vnode: any, container) {
-  const instance = createComponentInstance(vnode);
+function mountComponent(initialVNode: any, container) {
+  const instance = createComponentInstance(initialVNode);
 
   setupComponent(instance);
 
-  setupRenderEffect(instance, container);
+  setupRenderEffect(instance, initialVNode, container);
 }
-function setupRenderEffect(instance: any, container) {
-  const subTree = instance.render();
+
+function setupRenderEffect(instance: any, initialVNode, container) {
+  const { proxy } = instance;
+  const subTree = instance.render.call(proxy);
 
   // vnode -> patch
   // vnode -> element -> mountElement
 
   patch(subTree, container);
+
+  initialVNode.el = subTree.el;
+}
+
+function processElement(vnode, container) {
+  mountElement(vnode, container);
+}
+
+function mountElement(vnode: any, container: any) {
+  const el = (vnode.el = document.createElement(vnode.type));
+  const { children, props, shapeFlag } = vnode;
+
+  if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
+    el.textContent = children;
+  } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+    mountChildren(vnode, el);
+  }
+
+  for (const key in props) {
+    const val = props[key];
+    el.setAttribute(key, val);
+  }
+
+  container.append(el);
+}
+
+function mountChildren(vnode, container) {
+  vnode.children.forEach((v) => {
+    patch(v, container);
+  });
 }
