51e458e7275abac8e158d0ac0844112f2c24ce7e
aha <1900461953@qq.com>
2023-01-09 10:51:07 +0800

    chore: 打包内容提交

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -1,7 +1,7 @@
 import { h } from "../../lib/guide-mini-vue.esm.js";
 export const App = {
   render() {
-    return h("div", "hi," + this.msg);
+    return h("div", {}, [h("p", {}, "hi," + this.msg), h("p", {}, "hi,mini-vue")]);
   },
 
   setup() {
