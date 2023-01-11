0034c859a874e3170b2667f003b9ee5e6e381b6d
aha <1900461953@qq.com>
2023-01-11 15:01:03 +0800

    feat: 事件注册的处理

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -58,7 +58,14 @@ function mountElement(vnode: any, container: any) {
 
   for (const key in props) {
     const val = props[key];
-    el.setAttribute(key, val);
+
+    const isOn = (key) => /^on[A-Z]/.test(key);
+    if (isOn(key)) {
+      const event = key.slice(2).toLowerCase();
+      el.addEventListener(event, val);
+    } else {
+      el.setAttribute(key, val);
+    }
   }
 
   container.append(el);
