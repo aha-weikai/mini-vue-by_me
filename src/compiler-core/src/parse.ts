7ea666db9e9b697151cd8c48294021017b02c1b2
someone-cool <weikai999828@gmail.com>
2023-01-17 16:05:17 +0800

    feat: 实现解析element

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from "./ast";
 
+const enum TagType {
+  Start,
+  End,
+}
+
 export function baseParse(content: string) {
   const context = createParseContext(content);
 
@@ -8,12 +13,42 @@ export function baseParse(content: string) {
 
 function parseChildren(context) {
   let node;
-  if (context.source.startsWith("{{")) {
+  let s = context.source;
+  if (s.startsWith("{{")) {
     node = parseInterpolation(context);
+  } else if (s[0] === "<") {
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context);
+    }
   }
   const nodes: any = [];
   nodes.push(node);
   return nodes;
+}
+
+function parseElement(context) {
+  // 解析tag
+
+  const element = parseTag(context, TagType.Start);
+  parseTag(context, TagType.End);
+
+  return element;
+}
+
+function parseTag(context: any, type: TagType) {
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+  const tag = match[1];
+
+  // 推荐字节流
+  advanceBy(context, match[0].length);
+  advanceBy(context, 1);
+
+  if (type === TagType.End) return;
+
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+  };
 }
 
 // Interpolation : 插值
